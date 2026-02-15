975d967cb8c3953f672c825d301eebde6d7aa1d8
tamunagg <tgoginashvili15@gmail.com>
2026-02-16 03:43:32 +0400

    Fix: header component

---
+++ src/components/HeaderComponent/Header.tsx
@@ -0,0 +1,34 @@
+import { ChevronDown, Menu, X } from 'lucide-react';
+import styles from './Header.module.css'
+import profilePic from '../../assets/ProfilePicture.png'
+import { useState } from 'react';
+
+function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    return (
+        <div className={styles.container} >
+            <div className={styles.burger} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
+            </div>
+
+            <div className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>                
+                <a href="#">Movies</a>
+                <a href="#">Series</a>
+                <a href="#">TV Series</a>
+            </div>
+
+            <h1>Film<span className={styles.highlight}>Box</span></h1>
+
+            <div className={styles.profile}>
+                <ChevronDown className={styles.arrowIcon} />
+
+                <p className={styles.profileName}>Arlene McCoy</p>
+
+                <img className={styles.profileImage} src={profilePic} alt="profile picture"/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
